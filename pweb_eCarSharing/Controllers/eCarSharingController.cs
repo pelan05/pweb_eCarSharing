8566d8b3320e6388671ac4531a2cffa2c11a8d63
Pedro Lanhoso <pelan05@gmail.com>
2020-01-03 20:51:22 +0000

    List views are working, RentVehicle view is WIP

--- pweb_eCarSharing/Controllers/eCarSharingController.cs
+++ pweb_eCarSharing/Controllers/eCarSharingController.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using pweb_eCarSharing.Models;
 
 namespace pweb_eCarSharing.Controllers
 {
     public class eCarSharingController : Controller
     {
+        ApplicationDbContext db = new ApplicationDbContext();
 
         // GET: eCarSharing
         public ActionResult Main()
@@ -19,17 +21,75 @@ namespace pweb_eCarSharing.Controllers
         public ActionResult CarStationList()
         {
             //TODO check list data
+            // var userId = User.Identity.GetUserId();
+            var carStationList = from m in db.CarStations
+                         select m;
+            return View(carStationList.ToList());
+        }
+
+        public ActionResult AddVehicle()
+        {
+            //TODO add view
+            //TODO check list data
+            return View();
+        }
+
+        public ActionResult AddCarStation()
+        {
+            //TODO add view
+            //TODO check list data
             return View();
         }
 
         public ActionResult VehicleList()
         {
             //TODO check list data
-            return View();
+            var carList = from m in db.Vehicles
+                          select m;
+            return View(carList.ToList());
         }
 
         public ActionResult RentVehicle()
         {
+
+            /*
+             
+        //
+        // GET: /Manage/AddPhoneNumber
+        public ActionResult AddPhoneNumber()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Manage/AddPhoneNumber
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> AddPhoneNumber(AddPhoneNumberViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            // Generate the token and send it
+            var code = await UserManager.GenerateChangePhoneNumberTokenAsync(User.Identity.GetUserId(), model.Number);
+            if (UserManager.SmsService != null)
+            {
+                var message = new IdentityMessage
+                {
+                    Destination = model.Number,
+                    Body = "Your security code is: " + code
+                };
+                await UserManager.SmsService.SendAsync(message);
+            }
+            return RedirectToAction("VerifyPhoneNumber", new { PhoneNumber = model.Number });
+        }
+
+             */
+
+
+
+
             // has to be a post?
             //TODO rent form
             return View();
@@ -38,7 +98,9 @@ namespace pweb_eCarSharing.Controllers
         public ActionResult ListRentalData()
         {
             //TODO check list data
-            return View();
+            var reservationList = from m in db.Reservations
+                          select m;
+            return View(reservationList.ToList());
         }
 
         public ActionResult ChangeRentalData()
@@ -48,20 +110,25 @@ namespace pweb_eCarSharing.Controllers
         }
 
         public ActionResult UsageStats()
-        {//TODO stats logic
-
+        {   //TODO stats logic
 
             //Most used vehicle type
-
+            var mostUsedType = from m in db.Reservations
+                                  select m.Vehicle;
             ViewBag.mostUsedType = "mockType";
+
             //Most used vehicle stations
-            
+            var mostUsedStationName = from m in db.Reservations
+                               select m.Vehicle;
+            var mostUsedStationLocation = from m in db.Reservations
+                               select m.Vehicle;
             ViewBag.mostUsedStationName = "mockStationName";
             ViewBag.mostUsedStationLocation = "mockStationLocation";
+
             //usage times in avg
-            
+            var avgUsageTime = from m in db.Reservations
+                               select m.Vehicle;
             ViewBag.avgUsageTime = "mockAvgTime";
-            
             
             return View();
         }
