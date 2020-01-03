7922b90ea0939b462890935b1139a70494973600
Pedro Lanhoso <pelan05@gmail.com>
2020-01-03 11:31:46 +0000

    Added initial views template

--- pweb_eCarSharing/App_Start/RouteConfig.cs
+++ pweb_eCarSharing/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace pweb_eCarSharing
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "eCarSharing", action = "Main", id = UrlParameter.Optional }
             );
         }
     }
---
+++ pweb_eCarSharing/Controllers/eCarSharingController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace pweb_eCarSharing.Controllers
+{
+    public class eCarSharingController : Controller
+    {
+
+        // GET: eCarSharing
+        public ActionResult Main()
+        {
+            //TODO fill with sample text
+            return View();
+        }
+
+        public ActionResult CarStationList()
+        {
+            //TODO check list data
+            return View();
+        }
+
+        public ActionResult VehicleList()
+        {
+            //TODO check list data
+            return View();
+        }
+
+        public ActionResult RentVehicle()
+        {
+            // has to be a post?
+            //TODO rent form
+            return View();
+        }
+
+        public ActionResult ListRentalData()
+        {
+            //TODO check list data
+            return View();
+        }
+
+        public ActionResult ChangeRentalData()
+        {
+            //TODO change rent data form
+            return View();
+        }
+
+        public ActionResult UsageStats()
+        {//TODO stats logic
+
+
+            //Most used vehicle type
+
+            ViewBag.mostUsedType = "mockType";
+            //Most used vehicle stations
+            
+            ViewBag.mostUsedStationName = "mockStationName";
+            ViewBag.mostUsedStationLocation = "mockStationLocation";
+            //usage times in avg
+            
+            ViewBag.avgUsageTime = "mockAvgTime";
+            
+            
+            return View();
+        }
+
+
+
+    }
+}
