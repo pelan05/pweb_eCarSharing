99c6843106924a59933cc912565526106c1eeac3
Pedro Lanhoso <pelan05@gmail.com>
2020-01-04 09:18:19 +0000

    Controllers added to be semanthically correct (each model has it's own controller)

---
+++ pweb_eCarSharing/Controllers/CarStationController.cs
@@ -0,0 +1,35 @@
+﻿using pweb_eCarSharing.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace pweb_eCarSharing.Controllers
+{
+
+    public class CarStationController : Controller
+    {
+        ApplicationDbContext db = new ApplicationDbContext();
+        public ActionResult CarStationList()
+        {
+            //TODO check list data
+            // var userId = User.Identity.GetUserId();
+            var carStationList = from m in db.CarStations
+                                 orderby m.stationCity
+                                 select m;
+            return View(carStationList.ToList());
+        }
+
+        public ActionResult AddCarStation()
+        {
+            //TODO add view
+            //TODO check list data
+            return View();
+        }
+
+
+
+
+    }
+}
---
+++ pweb_eCarSharing/Controllers/VehicleController.cs
@@ -0,0 +1,36 @@
+﻿using pweb_eCarSharing.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace pweb_eCarSharing.Controllers
+{
+    public class VehicleController : Controller
+    {
+
+        ApplicationDbContext db = new ApplicationDbContext();
+
+        public ActionResult AddVehicle()
+        {
+            //TODO add view
+            //TODO check list data
+            return View();
+        }
+
+
+        public ActionResult VehicleList()
+        {
+            //TODO check list data
+            var carList = from m in db.Vehicles
+                          select m;
+            return View(carList.ToList());
+        }
+
+
+
+
+
+    }
+}
